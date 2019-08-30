dd47da1172abc39680cdbb001794870de2c860f9
Vasanth V <vasanthv@outlook.com>
2019-08-30 14:45:05 +0530

    Removed reload options

--- main.js
+++ main.js
@@ -124,10 +124,10 @@ const template = [
 	{
 		label: 'View',
 		submenu: [
-			{ role: 'reload' },
-			{ role: 'forcereload' },
-			{ role: 'toggledevtools' },
-			{ type: 'separator' },
+			// { role: 'reload' },
+			// { role: 'forcereload' },
+			// { role: 'toggledevtools' },
+			// { type: 'separator' },
 			{ role: 'resetzoom' },
 			{ role: 'zoomin' },
 			{ role: 'zoomout' },
